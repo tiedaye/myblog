178b58a2212c1870b2b7a15518d29b32ecb4a4d7
zhangshuang-h <781792980@qq.com>
2018-10-21 17:04:40 +0800

    blog model

---
+++ application/models/Blog_model.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 2018/10/20
+ * Time: 15:17
+ */
+class Blog_model extends CI_Model{
+    public function get_blogdata(){
+        //$query = $this->db->get('t_blog');
+
+//        $this->db->select('*');
+//        $this->db->from('t_blog b');
+//        $this->db->join('t_blog_catalog c', 'c.catalog_id = b.catalog_id');
+//        $this->db->order_by("b.post_time",'DESC');
+//        $query = $this->db->get();
+        $sql = "select *,
+(SELECT count(*) from t_comment t WHERE t.blog_id = b.blog_id) num
+from t_blog b,t_blog_catalog c WHERE b.catalog_id = c.catalog_id
+order by b.post_time DESC";
+        $query = $this->db->query($sql);
+        return $query->result();
+        //获得数组形式
+    }
+    public function get_catalogdata(){
+        $query = $this->db->get('t_blog_catalog');
+
+        return $query->result();
+        //获得数组形式
+    }
+}
