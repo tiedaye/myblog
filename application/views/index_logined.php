2374b48d63abc7b05663f639637886ee3a0c54d4
zhangshuang-h <781792980@qq.com>
2018-10-28 22:02:27 +0800

    登录后重刷新的页面

---
+++ application/views/index_logined.php
@@ -0,0 +1,125 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xml:lang="zh-CN" xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN"><head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="Content-Language" content="zh-CN">
+  <title>Johnny的博客 - SYSIT个人博客</title>
+    <base href="<?php echo site_url();?>">
+      <link rel="stylesheet" href="css/space2011.css" type="text/css" media="screen">
+  <link rel="stylesheet" type="text/css" href="css/jquery.css" media="screen">
+  <script type="text/javascript" src="js/jquery-1.js"></script>
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/jquery_002.js"></script>
+  <script type="text/javascript" src="js/oschina.js"></script>
+  <style type="text/css">
+    body,table,input,textarea,select {font-family:Verdana,sans-serif,宋体;}	
+  </style>
+</head>
+<body>
+<!--[if IE 8]>
+<style>ul.tabnav {padding: 3px 10px 3px 10px;}</style>
+<![endif]-->
+<!--[if IE 9]>
+<style>ul.tabnav {padding: 3px 10px 4px 10px;}</style>
+<![endif]-->
+<div id="OSC_Screen"><!-- #BeginLibraryItem "/Library/OSC_Banner.lbi" -->
+<?php include "header.php"?>
+<div class="BlogList">
+    <ul>
+        <?php foreach ($blog_list as $list){ ?>
+            <li class='Blog' id='<?php  echo $list->blog_id?>'>
+
+                <h2 class='BlogAccess_true BlogTop_0'><a href="viewPost_comment.htm"><?php echo $list->blog_title?></a></h2>
+
+                <div class='outline'>
+
+                    <span class='time'><?php echo date("Y年m月d日 h:ia", strtotime($list->post_time))?></span>
+
+                    <span class='catalog'>分类: <a href="?catalog=92334"><?php echo $list->catalog_name?></a></span>
+
+                    <span class='stat'>统计: <?php echo $list->num?>评/<?php echo $list->checked?>阅</span>
+
+                </div>
+
+                <div class='TextContent' id='blog_content_24027'>
+
+                    <?php echo $list->blog_content?>
+
+                    <div class='fullcontent'><a href="viewPost_comment.htm">阅读全文...</a></div>
+
+                </div>
+
+            </li>
+        <?php } ?>
+    </ul>
+<div class="clear"></div>
+	</div>
+<div class="BlogMenu"><div class="admin SpaceModule">
+  <strong>博客管理</strong>
+  <ul class="LinkLine">
+	<li><a href="newBlog.htm">发表博客</a></li>
+			<li><a href="blogCatalogs.htm">博客分类管理</a></li>
+	<li><a href="blogs.htm">文章管理</a></li>
+	<li><a href="blogComments.htm">网友评论管理</a></li>
+  </ul>
+</div>
+<div class="catalogs SpaceModule">
+  <strong>博客分类</strong>
+  <ul class="LinkLine">
+    	<li><a href="#">工作日志(3)</a></li>
+		<li><a href="#">日常记录(0)</a></li>
+		<li><a href="#">转贴的文章(0)</a></li>
+	  </ul>
+</div>
+<div class="comments SpaceModule">
+  <strong>最新网友评论</strong>
+      <ul>
+		<li>
+		<span class="u"><a href="#"><img src="images/portrait.gif" alt="Johnny" title="Johnny" class="SmallPortrait" user="154693" align="absmiddle"></a></span>
+		<span class="c">hoho
+			<span class="t">发布于 11分钟前 <a href="viewPost_comment.htm">查看»</a></span>
+		</span>
+		<div class="clear"></div>
+	</li>
+		<li>
+		<span class="u"><a href="#"><img src="images/portrait.gif" alt="Johnny" title="Johnny" class="SmallPortrait" user="154693" align="absmiddle"></a></span>
+		<span class="c">测试评论111
+			<span class="t">发布于 34分钟前 <a href="viewPost_logined.htm">查看»</a></span>
+		</span>
+		<div class="clear"></div>
+	</li>
+		<li>
+		<span class="u"><a href="#"><img src="images/portrait.gif" alt="Johnny" title="Johnny" class="SmallPortrait" user="154693" align="absmiddle"></a></span>
+		<span class="c">测试评论
+			<span class="t">发布于 34分钟前 <a href="viewPost_logined.htm">查看»</a></span>
+		</span>
+		<div class="clear"></div>
+	</li>
+	  </ul>
+  </div></div>
+<div class="clear"></div>
+<script type="text/javascript" src="js/brush.js"></script>
+<link type="text/css" rel="stylesheet" href="css/shCore.css">
+<link type="text/css" rel="stylesheet" href="css/shThemeDefault.css">
+<script type="text/javascript"><!--
+$(document).ready(function(){
+	SyntaxHighlighter.config.clipboardSwf = '/js/syntax-highlighter-2.1.382/scripts/clipboard.swf';
+	SyntaxHighlighter.all();
+});
+//-->
+</script>
+<script type="text/javascript">
+<!--
+function delete_blog(blog_id){
+if(!confirm("文章删除后无法恢复，请确认是否删除此篇文章？")) return;
+ajax_post("/action/blog/delete?id="+blog_id,"",function(html){
+	$('#blog_'+blog_id).fadeOut();
+});
+}
+//-->
+</script></div>
+	<div class="clear"></div>
+	<div id="OSC_Footer">© 赛斯特(WWW.SYSIT.ORG)</div>
+</div>
+</div>
+</body></html>
