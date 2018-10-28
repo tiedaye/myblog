a45925f4277f38bfa035e570cda061272ab57e04
zhangshuang-h <781792980@qq.com>
2018-10-28 22:01:54 +0800

    首页

--- application/views/index.php
+++ application/views/index.php
@@ -2,7 +2,7 @@
 <html xml:lang="zh-CN" xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN"><head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="Content-Language" content="zh-CN">
-<!--    <base href="--><?php //echo site_url()?><!--">-->
+    <base href="<?php echo site_url()?>">
     <title>Johnny的博客 - SYSIT个人博客</title>
     <link rel="stylesheet" href="css/space2011.css" type="text/css" media="screen">
     <link rel="stylesheet" type="text/css" href="css/jquery.css" media="screen">
@@ -24,41 +24,7 @@
 <div id="OSC_Screen"><!-- #BeginLibraryItem "/Library/OSC_Banner.lbi" -->
     <div id="OSC_Banner">
         <div id="OSC_Slogon">Johnny's Blog</div>
-        <div id="OSC_Channels">
-            <ul>
-                <li><a href="#" class="project">心情 here...</a></li>
-            </ul>
-        </div>
-        <div class="clear"></div>
-    </div><!-- #EndLibraryItem --><div id="OSC_Topbar">
-        <div id="VisitorInfo">
-            当前访客身份：
-            游客 [ <a href="login.htm">登录</a> | <a href="reg.htm">注册</a> ]
-            <span id="OSC_Notification">
-			<a href="inbox.htm" class="msgbox" title="进入我的留言箱">你有<em>0</em>新留言</a>
-					</span>
-        </div>
-        <div id="SearchBar">
-            <form action="Search">
-                <input name="user" value="154693" type="hidden">
-                <input id="txt_q" name="q" class="SERACH" value="在此空间的博客中搜索" onblur="(this.value=='')?this.value='在此空间的博客中搜索':this.value" onfocus="if(this.value=='在此空间的博客中搜索'){this.value='';};this.select();" type="text">
-                <input class="SUBMIT" value="搜索" type="submit">
-            </form>
-        </div>
-        <div class="clear"></div>
-    </div>
-    <div id="OSC_Content"><div class="SpaceChannel">
-            <div id="portrait"><a href="adminIndex.htm"><img src="images/portrait.gif" alt="Johnny" title="Johnny" class="SmallPortrait" user="154693" align="absmiddle"></a></div>
-            <div id="lnks">
-                <strong>Johnny的博客</strong>
-                <div>
-                    <a href="#">TA的博客列表</a>&nbsp;|
-                    <a href="sendMsg.htm">发送留言</a>
-                    </span>
-                </div>
-            </div>
-            <div class="clear"></div>
-        </div>
+            <?php include "header.php"?>
         <div class="BlogList">
             <ul>
                 <?php foreach ($blog_result as $list){ ?>
